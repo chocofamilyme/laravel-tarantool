ba0b374579fb67b58b49d6f0589dbcbf21c8b059
Kamenev <kamenev.n@gmail.com>
2019-08-08 15:22:07 +0500

    add PHPDoc

--- src/Traits/Helper.php
+++ src/Traits/Helper.php
@@ -4,6 +4,12 @@ namespace Chocofamily\Tarantool\Traits;
 
 trait Helper
 {
+    /**
+     * Get type of SQL query
+     *
+     * @param  string  $sql
+     * @return string
+     */
     public function getSqlType(string $sql): string
     {
         $sql = trim($sql);
